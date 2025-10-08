872d614ddcd28c7f5cd702532c900ae0416da20a
mazendrop <mazen8abdallah@gmail.com>
2025-10-08 11:02:24 +0200

    Test2

--- app/src/main/java/org/example/TestToro.java
+++ app/src/main/java/org/example/TestToro.java
@@ -4,7 +4,7 @@ public class TestToro {
 
     public static void main(String[] args) {
 
-        System.out.println("Hallo");
+        System.out.println("Welc To Programmierung Zwei (2)");
 
     }
 
